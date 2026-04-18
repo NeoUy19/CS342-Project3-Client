4c484739f46520ac5020fc2dee70c48f503a225e
Adi <adideenlatic@gmail.com>
2026-04-18 15:08:48 -0500

    Created/built Client.java. Added a login UI and UI for current players

--- src/main/java/ClientGUI.java
+++ src/main/java/ClientGUI.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -18,11 +17,17 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class ClientGUI extends Application {
-    HashMap<String, Scene> sceneMap;
-    private TextField usernameField, messageField;
-    private Button sendButton, playButton, howtoplay, signinButton;
+    HashMap<String, Scene> sceneMap, loginMap;
+    private TextField usernameField, messageField, loginField;
+    private Button sendButton, playButton, howtoplay, signinButton, messageButton;
+    VBox clientList, loginVbox;
+    HBox userBox;
+    Client clientConnection;
+    Label loginLabel, userLabel;
+    String currentUser;
     BorderPane root;
     GridPane board;
+    ScrollPane userPane;
 
     ListView<String> messagesList;
     public static void main(String[] args) {
@@ -31,10 +36,54 @@ public class ClientGUI extends Application {
 
 
     public void start(Stage primaryStage) {
+
+         clientConnection = new Client(data -> {    //Creating the new Client
+            Platform.runLater(()-> {
+                if (((Message) data).getMsgType().equals(Message.error)) {          //Conditions for different server messages
+                    loginLabel.setText("Username is already Taken!");
+                } else if (((Message) data).getMsgType().equals(Message.userList)) {
+                    currentUser = loginField.getText();
+                    clientList.getChildren().clear();       //Clear the Vbox and repopulate it everytime a new user joins
+                        for(String username : ((Message) data).getGroupMembers()) {
+                            if(username.equals(currentUser)){      //This makes it so if its your screen you won't see the play btn on ur name
+                                userLabel = new Label();
+                                userLabel.setText(username);
+                                userBox = new HBox(userLabel);
+                                clientList.getChildren().add(userBox);
+                            } else {
+                                userLabel = new Label();        //Displays current players with a play btn and message btn
+                                userLabel.setText(username);
+                                playButton = new Button("Play");
+                                messageButton = new Button("Message");
+                                userBox = new HBox(userLabel, playButton, messageButton);
+                                clientList.getChildren().add(userBox);
+                            }
+                        }
+                    if(!loginField.getText().isEmpty() && ((Message) data).getGroupMembers().contains(loginField.getText())){   //Check to see if username is taken
+                        currentUser = ((Message) data).getClient();     //If not switch scenes and thats your user
+                        primaryStage.setScene(sceneMap.get("home"));
+                        primaryStage.setTitle("Client");
+                        primaryStage.show();
+                 }
+                } else if (((Message) data).getMsgType().equals(Message.serverMessage)) {
+
+                } else if (((Message) data).getMsgType().equals(Message.challenge)) {
+
+                }
+            });
+        });
+
+        clientConnection.start();
+
+        loginMap = new HashMap<String, Scene>();
+        loginMap.put("login", loginGUI());
+
+        primaryStage.setScene(loginMap.get("login"));
+        primaryStage.setTitle("Client");
+        primaryStage.show();
+
         sceneMap = new HashMap<String, Scene>();
         sceneMap.put("home",  createHomeGUI());
-//        sceneMap.put("CreateUser",  createUserGui());
-//        clientConnection.start();
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent t) {
@@ -42,26 +91,42 @@ public class ClientGUI extends Application {
                 System.exit(0);
             }
         });
-
-
-        primaryStage.setScene(sceneMap.get("home"));
-        primaryStage.setTitle("Client");
-        primaryStage.show();
     }
 
     public Scene createHomeGUI(){
         root = new BorderPane();
         board = new GridPane();
+        //clientList = new VBox();
         for (int row = 0 ; row < 8; row++){
             for (int col = 0 ; col < 8; col++){
                 board.add(buildSquare(row,col),col,row);
             }
         }
-        playButton = new Button("Play");
-        howtoplay = new Button("How to Play");
-        VBox rightSide = new VBox(playButton, howtoplay);
+        clientList = new VBox();
+        userPane = new ScrollPane(clientList);      //created a scroll pane and itll adjust accordingly
+        userPane.setFitToWidth(true);
         root.setLeft(board);
-        root.setRight(rightSide);
+        root.setRight(userPane);
+        return new Scene(root, 800, 600);
+    }
+
+    public Scene loginGUI(){   //Login Scene
+        root = new BorderPane();
+
+        loginField = new TextField();
+        signinButton = new Button("Sign in");
+        loginLabel = new Label();
+
+        signinButton.setOnAction(e->{
+            Message userName = new Message(Message.createUser, loginField.getText());       //Gets the username and stores it
+            clientConnection.send(userName);
+        });
+
+        loginVbox = new VBox(10, loginField, signinButton, loginLabel);
+        root.setCenter(loginVbox);
+        loginVbox.setAlignment(Pos.CENTER);
+        loginVbox.setMaxWidth(300);
+
         return new Scene(root, 800, 600);
     }
 
